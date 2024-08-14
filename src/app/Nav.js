ce519a73a89fdbd92ed39e43392bf47fd8f7a82e
poornani11 <poorani.mgr@gmail.com>
2024-08-14 15:34:44 +0100

    Initial commit

---
+++ src/app/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import './Nav.css';
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener('scroll', transitionNavBar);
+  }, [])
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__contents">
+        <img src="https://www.pngall.com/wp-content/uploads/13/Netflix-Logo-PNG.png" className="nav__logo" alt="netflix-logo" />
+        <img src="https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg"
+          className="nav__avatar" alt="profile-icon" />
+      </div>
+    </div>
+  )
+}
+
+export default Nav;
---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB3wT3b2AjmFO7mTscAYwwP36y3tsNtcu0",
+    authDomain: "netflix-clone-poorni.firebaseapp.com",
+    projectId: "netflix-clone-poorni",
+    storageBucket: "netflix-clone-poorni.appspot.com",
+    messagingSenderId: "157843041797",
+    appId: "1:157843041797:web:d77d11591c46305dc5f66c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
