a4de8826e001597fa3e5dc4e453c95f1c7762af2
poornani11 <poorani.mgr@gmail.com>
2024-08-15 14:32:38 +0100

    Added the home page

--- src/app/Banner.js
+++ src/app/Banner.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from '../axios';
+import requests from '../Requests';
 import './Banner.css'
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
   }
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOrginals);
+      setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+      return request;
+    }
+    fetchData()
+  }, []);
+
+  console.log(movie);
   return (
     <div className="banner" style={{
       backgroundSize: "cover",
-      backgroundImage: `url("https://www.acemetrix.com/wp-content/uploads/2015/08/black-banner-vector21.png")`,
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.
+        backdrop_path
+        }")`,
       backgroundPosition: "center center",
     }}>
       <div className="banner__contents">
         <h1 className="banner__title">
-          Movie Name
+          {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(`This is a test description`, 150)}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB3wT3b2AjmFO7mTscAYwwP36y3tsNtcu0",
     authDomain: "netflix-clone-poorni.firebaseapp.com",
+    databaseURL: "https://netflix-clone-poorni-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "netflix-clone-poorni",
     storageBucket: "netflix-clone-poorni.appspot.com",
     messagingSenderId: "157843041797",
     appId: "1:157843041797:web:d77d11591c46305dc5f66c"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
